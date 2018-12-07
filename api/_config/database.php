a680d3e5c0f52be430731a785bc248c794caa9b1
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2018-12-07 10:40:13 +0100

    Init

---
+++ api/_config/database.php
@@ -0,0 +1,28 @@
+<?php
+
+    class Database{
+
+
+        private $host = "localhost";
+        private $db_name = "db_minska";
+        private $username = "root";
+        private $password = "";
+        public $conn;
+
+        public function connect(){
+
+            $this->conn = null;
+
+            try{
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            } catch(PDOException $exception){
+                echo "Connection error: " . $exception->getMessage();
+            }
+
+            return $this->conn;
+
+        }
+
+    }
+
+?>
