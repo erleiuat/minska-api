388d0634a131b76b207e080755505a71486b5659
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2019-01-17 15:03:31 +0100

    Major Updates tho

---
+++ template/delete/index.php
@@ -0,0 +1,48 @@
+<?php
+
+// ---- Include Defaults
+include_once '../../_config/headers.php';
+include_once '../../_config/database.php';
+include_once '../../_config/core.php';
+include_once '../../_config/libs/php-jwt-master/src/BeforeValidException.php';
+include_once '../../_config/libs/php-jwt-master/src/ExpiredException.php';
+include_once '../../_config/libs/php-jwt-master/src/SignatureInvalidException.php';
+include_once '../../_config/libs/php-jwt-master/src/JWT.php';
+use \Firebase\JWT\JWT;
+
+// ---- Initialize Default
+$database = new Database();
+$db = $database->connect();
+$data = json_decode(file_get_contents("php://input"));
+
+// ---- Include Object
+include_once '../../_config/objects/template.php';
+$template = new Template($db);
+// ---- End of default Configuration
+
+$token = isset($data->token) ? $data->token : "";
+
+if($token){
+
+    try {
+
+        $decoded = JWT::decode($token, $token_conf['secret'], $token_conf['algorithm']);
+        $template->userid = $decoded->data->id;
+        $template->id = $data->id;
+
+        try {
+            $template->delete();
+            returnSuccess();
+        } catch (Exception $e) {
+            returnError($e);
+        }
+
+    } catch(Exception $e) {
+        returnForbidden($e);
+    }
+
+} else {
+    returnBadRequest();
+}
+
+?>
