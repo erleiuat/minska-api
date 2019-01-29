94c465c64e958a5ad25c458e0df16c3385cfbc6e
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2019-01-29 21:53:43 +0100

    hoppla

--- _config/core.php
+++ _config/core.php
@@ -17,7 +17,7 @@ $token_conf = array(
 function authenticate(){
 
     if (isset(getallheaders()['Authorization'])) {
-        list($type, $data) = explode(" ", getallheaders()['authorization'], 2);
+        list($type, $data) = explode(" ", getallheaders()['Authorization'], 2);
         if (strcasecmp($type, "Bearer") == 0) {
             return $data;
         } else {
