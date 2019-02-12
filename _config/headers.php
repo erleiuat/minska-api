1a247c8af781922a181732e7806f0875fc265280
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2019-02-12 12:42:20 +0100

    ups

--- _config/headers.php
+++ _config/headers.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://localhost:8080");
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, GET");
 header("Access-Control-Allow-Credentials: true");
