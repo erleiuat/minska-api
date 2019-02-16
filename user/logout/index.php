937833c254da1f4e8f578429cd1bdccf00818d59
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2019-02-16 20:46:46 +0100

    Update index.php

--- user/logout/index.php
+++ user/logout/index.php
@@ -4,5 +4,5 @@ include_once '../../_config/headers.php';
 include_once '../../_config/core.php';
 
 $expire = time()-50;
-header("Set-Cookie: secureToken=null; Dexpires=$expire; Path=/; samesite=strict; httpOnly; $secure");
+header("Set-Cookie: secureToken=null; expires=$expire; Path=/; samesite=strict; httpOnly; $secure");
 returnSuccess($jwt);
