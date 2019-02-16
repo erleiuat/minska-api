776d9dc20bcc9c3652d6e65ea9f3c8d50899bf9e
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2019-02-16 20:47:43 +0100

    working logout

--- user/logout/index.php
+++ user/logout/index.php
@@ -3,6 +3,12 @@
 include_once '../../_config/headers.php';
 include_once '../../_config/core.php';
 
-$expire = time()-50;
-header("Set-Cookie: secureToken=null; expires=$expire; Path=/; samesite=strict; httpOnly; $secure");
+$domain = ".eliareutlinger.ch";
+//$domain = "localhost";
+
+$unset = "";
+
+header("Set-Cookie: appToken=$unset; Domain=$domain; expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; samesite=strict; $secure");
+header("Set-Cookie: secureToken=$unset; Domain=$domain; expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; samesite=strict; httpOnly; $secure", false);
+
 returnSuccess($jwt);
